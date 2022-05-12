0c65cca1d293322bcbda854ab4aa36762a7ed8df
jeffnyak@gmail.com <jeffnyak@gmail.com>
2022-05-12 17:37:08 +0300

    shpping cart component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
 
 
 function App() {
   return (
-    <Register/>
+    <Login/>
   );
 }
 
